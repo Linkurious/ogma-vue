f95ae1c8d2309c4030c862a8ced07004d3bb5a5f
Leo Nicolle <leo@linkurio.us>
2022-02-08 12:47:27 +0100

    documnent tools

--- src/components/tools/Snapping.vue
+++ src/components/tools/Snapping.vue
@@ -1,8 +1,24 @@
 <script>
 import toolMixin from "../../mixins/ToolMixin";
 const snapping = toolMixin("snapping", {});
+
+/**
+ * Handles snapping tool
+ * See [snapping](https://doc.linkurio.us/ogma/latest/api.html#Ogma-tools-snapping)
+ * @displayName Snapping
+ */
 export default {
   name: "Snapping",
-  mixins: [snapping]
+  props: {
+    /**
+     * Wether the tool is enabled or not.
+     */
+    enabled: { type: Boolean, default: false },
+    /**
+     * The [options](https://doc.linkurio.us/ogma/latest/api.html#Ogma-tools-snapping) to pass to snapping
+     */
+    options: { type: Object, default: () => ({}) },
+  },
+  mixins: [snapping],
 };
 </script>
