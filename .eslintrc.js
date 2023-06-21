21f9ce11c602ab7296e4647ba184781eda3aa199
Alexander Milevski <alexander@linkurio.us>
2023-06-21 14:57:22 +0200

    Updated license (#40)
    * Updated license
    
    * Fixed lint error
    
    * Another lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential"
-  ],
+  extends: ["eslint:recommended", "plugin:vue/vue3-essential"],
   parserOptions: {
     ecmaVersion: 2020,
   },
@@ -13,7 +11,18 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-unused-vars": "warn",
-    "quotes": "warn",
+    quotes: "warn",
     "no-unused-expressions": "warn",
   },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
 };
