4a17e5e4753fa58f9d8d2c7b9567185023e75587
Alexander Milevski <alexander@linkurio.us>
2023-06-21 14:53:33 +0200

    Another lint error

--- src/mixins/LayerMixin.js
+++ src/mixins/LayerMixin.js
@@ -1,4 +1,3 @@
-
 export default {
   name: "Layer",
   props: {
@@ -7,22 +6,22 @@ export default {
      */
     visible: { default: true },
     /**
-    * Level of the layer. See [Layer setLevel](https://doc.linkurio.us/ogma/latest/api.html#Layer)
-    * Infinity and -Infinity triggers moveToTop and moveToBottom.
-    */
+     * Level of the layer. See [Layer setLevel](https://doc.linkurio.us/ogma/latest/api.html#Layer)
+     * Infinity and -Infinity triggers moveToTop and moveToBottom.
+     */
     level: { default: 0 },
   },
   inject: ["ogma"],
   data() {
     return {
-      layer: null
+      layer: null,
     };
   },
   watch: {
     level: function (newValue) {
       if (newValue === Infinity) {
         this.layer.moveToTop();
-      } else if (value == -Infinity) {
+      } else if (newValue === -Infinity) {
         this.layer.moveToBottom();
       } else {
         this.layer.moveTo(newValue);
@@ -36,5 +35,5 @@ export default {
   unmounted() {
     this.layer.destroy();
     this.layer = null;
-  }
+  },
 };
