42a2e530352e56c73cb16e7a1c9e501f64da1934
Leo Nicolle <leo@linkurio.us>
2024-01-16 17:57:41 +0100

    Updates

--- assets/examples_ogma.md.WQas1OQp.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as i,c as a,R as n}from"./chunks/framework.q9FnI7o_.js";const o=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"examples/ogma.md","filePath":"examples/ogma.md"}'),h={name:"examples/ogma.md"},t=n("",1),k=[t];function l(p,E,e,r,d,g){return i(),a("div",null,k)}const F=s(h,[["render",l]]);export{o as __pageData,F as default};
---
+++ assets/examples_ogma.md.vfLecLst.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as i,c as a,R as n}from"./chunks/framework.6gCNz0yi.js";const o=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"examples/ogma.md","filePath":"examples/ogma.md"}'),h={name:"examples/ogma.md"},t=n("",1),k=[t];function l(p,E,e,r,d,g){return i(),a("div",null,k)}const F=s(h,[["render",l]]);export{o as __pageData,F as default};
